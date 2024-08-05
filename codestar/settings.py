48c487c2a29f864f937bfabe392ecee38e4a9f2c
Naomi Hancock <naomi_hancock@yahoo.co.uk>
2024-08-05 02:36:06 +0000

    add secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-mujxdp6ztj(%22bjjm*rg-&es$08dyf#_rfb^jrh)j*vy4^wz4'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -87,6 +88,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+
 }
 
 
